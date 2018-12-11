838db5f2a1b62760d44bad70002cb029a764200e
Auss <a.abbood@live.de>
2018-12-11 11:20:16 +0100

    Further split code from notebook to seperate .py files

---
+++ blizz_api_calls.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import pickle
+
+request = requests.get("https://us.api.blizzard.com/wow/realm/status?locale=en_US&access_token=US42MIyf36fGanr2BLbxvBmxxnbM5MB3Dy")
+blizz_realms_us = json.loads(request.content.decode('utf-8'))
+pickle.dump(blizz_realms_us, open('blizz_realms_us.p', 'wb'))
+
+
+request2 = requests.get("https://eu.api.blizzard.com/wow/realm/status?access_token=US42MIyf36fGanr2BLbxvBmxxnbM5MB3Dy")
+blizz_realms_eu = json.loads(request2.content.decode('utf-8'))
+pickle.dump(blizz_realms_eu, open('blizz_realms_eu.p', 'wb'))
--- clean_server_activity.py
+++ clean_server_activity.py
@@ -2,29 +2,22 @@ import re
 import pickle
 
 
-# Loads a dictionary where you get a tuple of server name and server activity as a pandas df by id
-wacraft_server_activity = pickle.load(open("crawled_server_activity.p", 'rb'))
+def clean():
+    # Loads a dictionary where you get a tuple of server name and server activity as a pandas df by id
+    wacraft_server_activity = pickle.load(open("crawled_server_activity.p", 'rb'))
 
-servername_and_dataframe = []
-for key in wacraft_server_activity:
-    server_name = wacraft_server_activity[key][0]
-    dataframe = wacraft_server_activity[key][1]
-    dataframe = dataframe.drop(["alliance_entries", 'horde_entries'], axis=1).astype(int, copy=False)
-    servername_and_dataframe.append((server_name, dataframe))
+    servername_and_dataframe = {}
+    for _, name_df_tuple in wacraft_server_activity.items():
+        server_name, dataframe = name_df_tuple
+        server_name = re.sub(r" - [A-Z]{2,}", '', server_name)  # Clean server name
+        dataframe = dataframe.drop(["alliance_entries", 'horde_entries'], axis=1)
+        dataframe = dataframe.sum(axis=1).astype(int, copy=False)
+        servername_and_dataframe[server_name] = dataframe
 
+    # Remove completely empty dataframes
+    empty_dfs = [key for key in servername_and_dataframe if
+                 servername_and_dataframe[key].isin([0]).all().all()]
+    for key in empty_dfs:
+        servername_and_dataframe.pop(key)
 
-# Get names from wacraftrealms.com in a cleaner way
-from_list_to_dict = servername_and_dataframe
-servername_and_dataframe = {}
-for server_df_tuple in from_list_to_dict:
-    cleaned_name = (re.sub(r" - [A-Z]{2,}", '', server_df_tuple[0]))
-    servername_and_dataframe[cleaned_name] = server_df_tuple[1]
-cleaned_server_names = [re.sub(r" - [A-Z]{2,}", '', server_name) for server_name,_ in from_list_to_dict]
-
-# Remove completely empty dataframes
-empty_dfs = [key for key in servername_and_dataframe if
-             servername_and_dataframe[key].isin([0]).all().all()]
-for key in empty_dfs:
-    servername_and_dataframe.pop(key)
-
-pickle.dump(servername_and_dataframe, open('cleaned_server_activity.p', 'wb'))
+    pickle.dump(servername_and_dataframe, open('cleaned_server_activity.p', 'wb'))
