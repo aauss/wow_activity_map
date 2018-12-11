6331f71b76face759d011030519e9b7d63ae6525
Auss <a.abbood@live.de>
2018-12-11 15:09:16 +0100

    Clean bynames added by wacraftrealms.com that are not found in Blizzard's names

--- clean_server_activity.py
+++ clean_server_activity.py
@@ -1,6 +1,6 @@
 import re
 import pickle
-
+import numpy as np
 
 def clean():
     # Loads a dictionary where you get a tuple of server name and server activity as a pandas df by id
@@ -10,7 +10,12 @@ def clean():
     for _, name_df_tuple in wacraft_server_activity.items():
         server_name, dataframe = name_df_tuple
         server_name = re.sub(r" - [A-Z]{2,}", '', server_name)  # Clean server name
-        dataframe = dataframe.drop(["alliance_entries", 'horde_entries'], axis=1)
+        if 'EU' in server_name:
+            server_name = re.sub(r"EU-", '', server_name)
+            server_name = re.sub(r'\(.*\)', '', server_name)
+        server_name = server_name.strip()
+        dataframe = dataframe.drop(["alliance_entries", 'horde_entries'], axis=1).replace(np.nan, 1)
+
         dataframe = dataframe.sum(axis=1).astype(int, copy=False)
         servername_and_dataframe[server_name] = dataframe
 
@@ -21,3 +26,4 @@ def clean():
         servername_and_dataframe.pop(key)
 
     pickle.dump(servername_and_dataframe, open('cleaned_server_activity.p', 'wb'))
+clean()
---
+++ format_for_visualization.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+cleaned_series = pickle.load(open('cleaned_server_activity.p', 'rb'))
+blizz_realms_us = pickle.load(open('blizz_realms_us.p', 'rb'))['realms']
+
+blizz_realms_and_timezone_us = [(blizz_realms_us[i]['name'], blizz_realms_us[i]['timezone'])
+                                for i in range(len(list(blizz_realms_us)))]
+blizz_realms_and_timezone_us = pd.DataFrame(blizz_realms_and_timezone_us, columns=['server_name', 'timezone'])
+timezone_dict = blizz_realms_and_timezone_us.groupby("timezone").groups
+
+# Create a dict with timezone as key and server names as values (indestead of index)
+timezone_dict_to_convert = deepcopy(timezone_dict)
+timezone_dict = {}
+for key in timezone_dict_to_convert.keys():
+    timezone_dict[key] = blizz_realms_and_timezone_us.iloc[
+        timezone_dict_to_convert[key].tolist()]['server_name'].tolist()
+
+averaged_timezone_dict = {}
+for timezone in timezone_dict:
+    running_avg = pd.Series(np.zeros(24))
+    for server in timezone_dict[timezone]:
+        try:
+            running_avg += cleaned_series[server]
+        except KeyError:
+            continue
+    averaged_timezone_dict[timezone] = running_avg / len(timezone_dict[timezone])
+
